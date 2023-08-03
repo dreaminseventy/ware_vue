3dac6d60126f8366bc18e8a3d2ec6e49c6a77b70
dreaminseventy <yang15983210406@163.com>
2023-08-03 14:40:41 +0800

    为方法添加注释，同时为新增添加规则

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,6 @@
 <template>
     <el-main style="height: 100%">
+        <!--查询界面-->
         <div style="padding-bottom: 5px">
             <el-input v-model="account" placeholder="请输入查询账号"
                       @keydown.enter.native="loadPost"
@@ -21,7 +22,7 @@
             <el-button type="warning" @click = "reset">重置</el-button>
             <el-button type="success" style="margin-left: 500px" @click="addNew" >新增</el-button>
         </div>
-
+        <!--中间查询数据展示界面-->
         <el-table :data="tableData"
         :header-cell-style="{ background:'#555d65',color:'#f2f2f2'}"
         border style="width: 100%"
@@ -66,7 +67,8 @@
                 <el-button size="small" type="danger">删除</el-button>
             </el-table-column>
         </el-table>
-            <el-pagination
+        <!--分页-->
+        <el-pagination
                 @size-change="handleSizeChange"
                 @current-change="handleCurrentChange"
                 :current-page="pageNum"
@@ -75,40 +77,41 @@
                 layout="total, sizes, prev, pager, next, jumper"
                 :total="total">
             </el-pagination>
+        <!--新增界面-->
         <el-dialog
             title="提示"
             :visible.sync="centerDialogVisible"
             width="30%"
             center>
-            <el-form ref="form" :model="form" label-width="80px">
-                <el-form-item label="账号">
+            <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+                <el-form-item label="账号" prop="account">
                     <el-col :span="20">
                     <el-input v-model="form.account"></el-input>
                     </el-col>
                 </el-form-item>
-                <el-form-item label="用户名">
+                <el-form-item label="用户名" prop="name">
                     <el-col :span="20">
                         <el-input v-model="form.name"></el-input>
                     </el-col>
                 </el-form-item>
-                <el-form-item label="密码">
+                <el-form-item label="密码" prop="password">
                     <el-col :span="20">
                         <el-input v-model="form.password"></el-input>
                     </el-col>
                 </el-form-item>
-                <el-form-item label="年龄">
+                <el-form-item label="年龄" prop="age">
                     <el-col :span="20">
                         <el-input v-model="form.age"></el-input>
                     </el-col>
                 </el-form-item>
-                <el-form-item label="性别">
+                <el-form-item label="性别" prop="sex">
                     <el-radio-group v-model="form.sex">
                         <el-radio label="1">男</el-radio>
                         <el-radio label="2">女</el-radio>
                         <el-radio label="3">沃尔玛购物袋</el-radio>
                     </el-radio-group>
                 </el-form-item>
-                <el-form-item label="电话">
+                <el-form-item label="电话" prop="phone">
                     <el-col :span="20">
                         <el-input v-model="form.phone"></el-input>
                     </el-col>
@@ -126,15 +129,37 @@
 export default {
     name: "wareMain",
     data() {
+        //对年龄大于150的进行重新输入
+        let checkAge = (rule,value,callback)=>{
+            if(value>150){
+                callback(new Error('太老了,请重试'));
+            }else {
+                callback();
+            }
+        };
+        //用于对账户是否以存在进行查询
+        let checkDuplicate =(rule,value,callbacke)=>{
+            if(this.form.id){
+                return callbacke
+            }
+            this.$axios.get(this.$http+'/user/findByAccount?account='+this.form.account).then(res=>res.data).then(res=>{
+                console.log(res)
+                if (res.code!==200){
+                   return callbacke
+                }else {
+                    return callbacke(new Error('账号已存在'))
+                }
+            })
+        };
         return {
-            tableData:[],
-            pageSize:5,
-            pageNum:1,
-            total:10,
-            name:'',
-            sex:'',
-            account:'',
-            sexs:[
+            tableData: [],//查询的内容在这里展示
+            pageSize: 5,
+            pageNum: 1,
+            total: 10,
+            name: '',
+            sex: '',
+            account: '',
+            sexs: [
                 {
                     value: 1,
                     label: '男'
@@ -152,34 +177,79 @@ export default {
                     label: ''
                 }
             ],
-            centerDialogVisible:false,
-            form:{
+            centerDialogVisible: false,//将新增表单设为不可见
+            //设置表单的form参数
+            form: {
                 account: '',
-                name:'',
-                password:'',
+                name: '',
+                password: '',
                 age: '',
-                sex:'3',
-                phone:''
+                sex: '3',
+                phone: ''
+            },
+            //为新增添加规则
+            rules: {
+                account: [
+                    {required: true, message: '请输入账号', trigger: 'blur'},
+                    {min: 3, max: 12, message: '长度在 3 到 12 个字符', trigger: 'blur'},
+                    {validator:checkDuplicate,trigger: 'blur'}
+                ],
+                name: [
+                    {required: true, message: '请输入用户名', trigger: 'blur'},
+                    {min: 3, max: 12, message: '长度在 3 到 12 个字符', trigger: 'blur'}
+                ],
+                password: [
+                    {required: true, message: '请输入密码', trigger: 'blur'},
+                    {min: 3, max: 12, message: '长度在 3 到 12 个字符', trigger: 'blur'}
+                ],
+                age: [
+                    {required: true, message: '请输入年龄', trigger: 'blur'},
+                    {min: 1, max: 3, message: '长度在 1 到 3 个字符', trigger: 'blur'},
+                    {pattern: /^([1-9][0-9]*){1,3}$/,message: '年龄必须为正整数',trigger: 'blur'},
+                    {validator:checkAge,trigger: 'blur'}
+                ],
+                phone: [
+                    {required: true, message: '手机号码不能为空', trigger: 'blur'},
+                    {pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,message: '请输入正确的手机号码',trigger: 'blur'}
+                ]
             }
         }
     },
     methods:{
+        //保存新增用户方法
         save(){
            this.centerDialogVisible=false
             this.$axios.post(this.$http+"/user/save ", this.form).then(res=>res.data).then(res=>{
                 console.log((res))
                 if (res.code===200){
-                    alert('新增成功')
+                    this.$message({
+                        showClose: true,
+                        message: '成功新增用户( •̀ ω •́ )y',
+                        type: 'success'
+                    });
                     this.loadGet()
                 }
                 else {
-                    alert("添加数据失败")
+                    this.$message({
+                        showClose: true,
+                        message: '添加用户失败，请重试(っ °Д °;)っ',
+                        type: 'success'
+                    });
                 }
             })
         },
+        //清除新增界面内容
+        resetForm() {
+            this.$refs.form.resetFields();
+        },
+        //显示新增界面，同时清除原有数据
         addNew(){
             this.centerDialogVisible=true
+            this.$nextTick(()=>
+            this.resetForm()
+            )
         },
+        //主要查询（使用get查询全部数据,包含分页查询）
         loadGet(){
             this.$axios.get(this.$http+'/user/page',{
                 params:{
@@ -196,6 +266,7 @@ export default {
                 }
             })
         },
+        //上方选择查询(使用post传递json数据对象，包含分页)
         loadPost(){
             if (this.name===''&&this.sex===''&&this.account===''){
                 return
@@ -218,6 +289,7 @@ export default {
                 }
                 })
         },
+        //分页查询，规定每页的条数
         handleSizeChange(val) {
             console.log(`每页 ${val} 条`);
             this.pageNum=1
@@ -230,6 +302,7 @@ export default {
                 }
 
         },
+        //分页查询，当前为第几页
         handleCurrentChange(val) {
             console.log(`当前页: ${val}`);
             this.pageNum=val
@@ -240,6 +313,7 @@ export default {
                 this.loadPost()
             }
         },
+        //重制查询框内容，并使页面返回初始状态
         reset(){
             this.sex=''
             this.name=''
@@ -248,7 +322,7 @@ export default {
             this.loadGet()
 
         }
-
+    //页面加载前先进行数据读取
     },
     beforeMount() {
         this.loadGet();
